4285b7c0353987ccd797bf8052bec091777eb894
David Duefrene <fortismilite@gmail.com>
2022-04-16 10:01:28 -0600

    switched from reading a local JSON file to pulling from a remot repo

--- src/store/actions/data.js
+++ src/store/actions/data.js
@@ -1,16 +1,20 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-import testData from './2020.json';
 import * as actions from './actionTypes';
 
 const LoadData = () => (dispatch) => {
-    dispatch({
-        type: actions.LOADDATA,
-        data: {
-            isLoading: false,
-            ...testData,
-        },
-    });
+    axios.get('https://api.github.com/repos/David-Duefrene/Colorado-Budget/contents/src/store/actions/2020.json')
+        .then((result) => {
+            dispatch({
+                type: actions.LOADDATA,
+                data: {
+                    isLoading: false,
+                    ...JSON.parse(atob(result.data.content)),
+                },
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
 };
 
 export const SetSubItem = (subItem) => (dispatch) => {
