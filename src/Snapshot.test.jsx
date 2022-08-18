c1cb2dc0f8deec40c46784a8c227f6fbdef3ff7f
David Duefrene <fortismilite@gmail.com>
2022-08-18 00:35:04 +0000

    moved render with provider to test util folder

--- src/Snapshot.test.jsx
+++ src/Snapshot.test.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
 import { expect, it, describe } from 'vitest';
-import { render } from '@testing-library/react';
 
 import store from './store';
 import Dashboard from './containers/Dashboard/Dashboard';
+import renderWithProvider from './testUtil/renderWithProvider';
 import testData from './dummyData.json';
-
-const renderWithProvider = (element) => render(<Provider store={store}>{element}</Provider>);
 
 describe('index', () => {
     it('renders the same snapshot without crashing', () => {
